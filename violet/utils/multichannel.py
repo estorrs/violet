512fe4fa47bdb3b399b2c7714d43d38e60bd4657
Erik <epstorrs@gmail.com>
2021-07-07 11:02:06 -0500

    added retiling and analysis for multichannel images

--- violet/utils/multichannel.py
+++ violet/utils/multichannel.py
@@ -21,3 +21,17 @@ def create_pseudocolor_image(img, dataset, channels, colors=None):
 
     new = (new / np.max(new)) * 255.
     return new.astype(np.uint8)
+
+
+def retile_multichannel_image(labels, pseudos):
+    rs, cs = zip(*[(int(l.split('_')[-2]), int(l.split('_')[-1]))
+                   for l in labels])
+
+    tile_height, tile_width, _ = pseudos[0].shape
+    retiled = np.zeros(((np.max(rs) + 1) * tile_height, (np.max(cs) + 1) * tile_width, 3))
+    for r, c, img in zip(rs, cs, pseudos):
+        r1, r2 = r * tile_height, (r + 1) * tile_height
+        c1, c2 = c * tile_width, (c + 1) * tile_width
+        retiled[r1:r2, c1:c2, :] = img
+
+    return retiled.astype(np.uint8)
